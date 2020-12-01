63dc415d8af7ab29de74662c53fdf27044fa6d3b
Will <will.dunwoody@gmail.com>
2020-12-01 20:50:29 +0100

    Added the grid functionality with a reset button to start a new grid.

--- java.js
+++ java.js
@@ -1,26 +1,52 @@
 
-let etchContainer = document.querySelector('.etchContainer');
-const x = 16;
-let y = x * x;
+const etchContainer = document.querySelector('.etchContainer');
+let x = 16
 
 function etchGrid() {
+    const y = x * x;
+    const measure = 640/x;
+
     for (let i = 0; i < y; i++) {
         let newDiv = document.createElement('div');
-          etchContainer.appendChild(newDiv)
+        newDiv.className = "sketch";
+        newDiv.style.width = `${measure}px`;
+        newDiv.style.height = `${measure}px`;
+        etchContainer.appendChild(newDiv);
+    }
+    sketch()
+}
+
+function sketch() {
+    let sketch = document.querySelectorAll('.sketch');
+    console.log(sketch)
+    sketch.forEach((sketch) => {
+    sketch.addEventListener("mouseover", function() {
+        sketch.style.background = 'black';
+    });
+});
+};
+
+function clear() {
+    while (etchContainer.firstChild) {
+        etchContainer.removeChild(etchContainer.firstChild);
+    }
+};
+
+function newGrid() {
+    x = (prompt("How big would you like?"))
+    if (x < 100) {
+        clear();
+        etchGrid(x);
+    } else {
+        alert("Number is too big, try again.")
     }
 }
 
-    
+const reset = document.getElementById('resetButton');
 
-/*
-let clearGrid = () => {
-    $('.grid').remove();
-}
-
-let newGrid = () => {
+reset.addEventListener("click", function() {
+    newGrid();
+})
 
 
-
-}Clas*/
-
-etchGrid(y)
+etchGrid();
